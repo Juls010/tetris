b77ab09579c28a3c1ac6b6b35ffc10c16865f394
Juls00 <juliagimena00@alumnos.cei.es>
2025-03-14 11:20:41 +0100

    update 14/03

--- test/tetris/tetrisTest.java
+++ test/tetris/tetrisTest.java
@@ -22,5 +22,25 @@ class TetrisTest {
 	void testInicialScoreIsZero() {
 		assertEquals(0, game.currentScore(), "The initial score must be 0.");
 	}
+
+	@Test
+	void testPieceCanMoveDown() {
+		Tetromino piece = new Tetromino(new Point[] { new Point(4,0), new Point(5,0) }, Color.RED);
+		assertTrue(board.canMove(piece, 0, 1), "The piece should move down");
+	}
+	
+	@Test
+	void testPieceCannotMoveOutsideLeft() {
+		Tetromino piece = new Tetromino (new Point[] { new Point(0,0), new Point (1,0)} , Color.BLUE);
+		assertFalse(board.canMove(piece, -1, 0), "The piece should not move from the left edge");
+	}
+	
+	/*@Test 
+	void  testLineClearIncrearseScore() {
+		for (int x = 0; x < 10; x++) {
+			board.grid[19][x] = Color.GREEN;
+		}
+		
+	}*/
 	
 }
